4719fec0fb7ee20e3ae3916b407524395d1ad074
Silzinc <jonasbloch85@gmail.com>
2023-09-29 15:06:31 +0200

    Defined eval and reduce methods.

---
+++ src/basic.rs
@@ -0,0 +1,30 @@
+use crate::{traits::RatioFracType, RatioFrac};
+
+impl<T: RatioFracType> RatioFrac<T>
+{
+	#[inline]
+	pub fn eval(&self, x: T) -> T
+	{
+		self.numerator.eval(x.clone()) / self.denominator.eval(x.clone())
+	}
+}
+
+impl<T: RatioFracType + polyx::traits::FloatLike> RatioFrac<T>
+{
+	#[inline]
+	pub fn reduce(&mut self)
+	{
+		let (reduced_numerator, reduced_denominator) =
+			polyx::Polynomial::cofactor_float(&mut self.numerator, &mut self.denominator);
+		// println!("{:?}\n{:?}\n", reduced_numerator, reduced_denominator);
+		self.numerator = reduced_numerator;
+		self.denominator = reduced_denominator;
+		let lc_inv = self.denominator[self.denominator.degree()].clone().inv();
+		for i in 0..=self.denominator.degree() {
+			self.denominator[i] = self.denominator[i].clone() * lc_inv.clone();
+		}
+		for i in 0..=self.numerator.degree() {
+			self.numerator[i] = self.numerator[i].clone() * lc_inv.clone();
+		}
+	}
+}
